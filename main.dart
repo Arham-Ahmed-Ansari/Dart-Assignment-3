e24205745d60ef4d5b00a1faa65410a562ff27e3
Arham-Ahmed-Ansari <arhamahmedansari92@gmail.com>
2022-10-28 22:10:17 +0500

    Assignment done

---
+++ main.dart
@@ -0,0 +1,61 @@
+import 'dart:ffi';
+import 'dart:svg';
+
+void main(List<String> args) {
+  // Question no 1:
+  // List<String> names = ["Arham", "Ahmed", "Umair", "Ibaad"];
+  // print(names);
+
+  // Question no 2:
+  // List<String> days = [];
+  // days.addAll([
+  //   "Monday",
+  //   "Tuesday",
+  //   "Wednesday",
+  //   "Thursday",
+  //   "Friday",
+  //   "Saturday",
+  //   "Sunday"
+  // ]);
+  // print(days);
+
+  //Question no 3:
+  // List<String> friendNames = [
+  //   "Arham",
+  //   "Umair",
+  //   "Ibaad",
+  //   "Ahmed",
+  //   "Wasif",
+  //   "Wasay",
+  //   "Ahris"
+  // ];
+  // List<String> days = [
+  //   "Monday",
+  //   "Tuesday",
+  //   "Wednesday",
+  //   "Thursday",
+  //   "Friday",
+  //   "Saturday",
+  //   "Sunday"
+  // ];
+
+  // List<dynamic> startWithA =
+  //     days.where((element) => element.startsWith("A")).toList();
+  // print(startWithA);
+
+  // Question no 4:
+
+  // var info = {
+  //   "Name": "Arham Ahmed Ansari",
+  //   "Address": "C-1 huma town karachi",
+  //   "Age": 20,
+  //   "Country": "Karachi"
+  // };
+  // info["country"] = "Islamabad";
+  // print(info);
+
+  // Question no 5:
+
+  Map<String, dynamic> info = {'Name': "Arham", 'Phone': 03131231575};
+  print(info);
+}
